b867d4a8ca01475174f9585a8b909892646ea058
Elena <lam00071@morris.umn.edu>
2021-02-08 14:00:54 -0600

    Update ShippingComponent TS
    1) Import CartService
    2) Inject the cart service in the ShippingComponent constructor().

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ShippingComponent {
 
-  constructor() { }
+  constructor(private cartService: CartService) {
+  }
+
+  shippingCosts = this.cartService.getShippingPrices();
 
 }
