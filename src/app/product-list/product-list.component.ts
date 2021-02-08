b919abcf9dd44b6da1d922daf218711ac3149a2b
Elena <lam00071@morris.umn.edu>
2021-02-08 03:59:15 -0600

    Update ProductListComponent TS
    Define behavior when users click the button. Same with share() method, window.alert will let users know they will be notified when the products go on sale.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,4 +13,8 @@ export class ProductListComponent {
   share(): void {
     window.alert('The product has been shared!');
   }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
