54e0a5abe90e424633fd3674365e80460a8e29a7
Elena <lam00071@morris.umn.edu>
2021-02-08 14:10:14 -0600

    Update CartComponent TS
    Define an onSubmit() method to process the form that allows users to submit their info (name & address).

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -18,4 +18,10 @@ export class CartComponent {
     private formBuilder: FormBuilder,
     ) {}
 
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
