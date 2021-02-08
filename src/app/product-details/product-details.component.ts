d52990c48b86754367851439bc729ed8fe1dcfb2
Elena <lam00071@morris.umn.edu>
2021-02-08 05:06:00 -0600

    Update ProductDetailsComponent TS
    1) Define the product property
    2) Inject ActivatedRoute into the constructor() by adding private route: ActivatedRoute as an argument within the constructor's parentheses.
    3) In the ngOnInit() method, extract the productId from the route parameters and find the corresponding product in the products array.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,9 +10,18 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product;
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
